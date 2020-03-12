086c12110a32c28d5385a0ffbe6113669e38a567
Tim <yjl5062@hotmail.com>
2020-03-12 22:44:27 +0800

    创建博客列表页

---
+++ components/Advert.js
@@ -0,0 +1,13 @@
+import '../static/styles/components/advert.css'
+import React from "react";
+const Advert = ()=>{
+  return (
+      <div className="advert-div comm-box">
+        <div><img src={'../static/imgs/Vuekoaad1.jpg' }style={{width:'100%'}} /></div>
+        <div><img src={'../static/imgs/flutterad2.jpg' } style={{width:'100%'}}/></div>
+        <div><img src={'../static/imgs/WechatIMG12.jpeg'}  style={{width:'100%'}}/></div>
+      </div>
+  )
+}
+
+export default Advert
---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../static/styles/components/footer.css'
+const Footer = () => {
+ return (
+  <div className="footer-div">
+   <div>系统由 React+Node+Ant Desgin驱动 </div>
+   <div>JSPang.com</div>
+  </div>
+ );
+}
+
+export default Footer;
